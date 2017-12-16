2066a771b119e673e7187ed52d5c674082371fb7
Deepak Yadav <trigodeepak@gmail.com>
2017-12-16 20:35:10 +0530

    Create day8_part2.py

---
+++ day8_part2.py
@@ -0,0 +1,53 @@
+import operator
+with open('input.txt') as f:
+    lines = f.readlines()
+    res = 0
+    d = {}
+    max_val= [0,0]
+    for s in lines:
+        s = s.strip().split()
+        if s[0] not in d:
+            d[s[0]] = 0
+        if s[4] not in d:
+            d[s[4]] = 0
+        if s[5] == '>':
+            if d[s[4]] > int(s[6]):
+                if s[1] == 'inc':
+                    d[s[0]] = d[s[0]] + int(s[2])
+                else:
+                    d[s[0]] = d[s[0]] - int(s[2])
+        elif s[5] == '<':
+            if d[s[4]] < int(s[6]):
+                if s[1] == 'inc':
+                    d[s[0]] = d[s[0]] + int(s[2])
+                else:
+                    d[s[0]] = d[s[0]] - int(s[2])
+        elif s[5] == '<=':
+            if d[s[4]] <= int(s[6]):
+                if s[1] == 'inc':
+                    d[s[0]] = d[s[0]] + int(s[2])
+                else:
+                    d[s[0]] = d[s[0]] - int(s[2])
+        elif s[5] == '>=':
+            if d[s[4]] >= int(s[6]):
+                if s[1] == 'inc':
+                    d[s[0]] = d[s[0]] + int(s[2])
+                else:
+                    d[s[0]] = d[s[0]] - int(s[2])
+        elif s[5] == '==':
+            if d[s[4]] == int(s[6]):
+                if s[1] == 'inc':
+                    d[s[0]] = d[s[0]] + int(s[2])
+                else:
+                    d[s[0]] = d[s[0]] - int(s[2])
+        elif s[5] == '!=':
+            if d[s[4]] != int(s[6]):
+                if s[1] == 'inc':
+                    d[s[0]] = d[s[0]] + int(s[2])
+                else:
+                    d[s[0]] = d[s[0]] - int(s[2])
+        b = max(d.iteritems(), key=operator.itemgetter(1))[0]
+        if max_val[1] < d[b]:
+            max_val[0] = b
+            max_val[1] = d[b]
+    print max_val
