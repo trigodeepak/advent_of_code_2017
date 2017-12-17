7846d63a9ec6d95d735b0536f702b7aa70aa9fcd
Deepak Yadav <trigodeepak@gmail.com>
2017-12-17 09:29:41 +0530

    Create day11_part1.py

---
+++ day11_part1.py
@@ -0,0 +1,26 @@
+import operator
+with open('input.txt') as f:
+    lines = f.readline()
+    lines = (lines.strip().split(','))
+    x=y=z=0
+    
+    for i in lines:
+        if i == 'ne':
+            y-=1
+            x+=1
+        elif i == 'se':
+            y-=1
+            z+=1
+        elif i == 's':
+            x-=1
+            z+=1
+        elif i == 'sw':
+            y+=1
+            x-=1
+        elif i == 'nw':
+            y+=1
+            z-=1
+        elif i == 'n':
+            x+=1
+            z-=1
+    print (abs(x)+abs(y)+abs(z))/2
