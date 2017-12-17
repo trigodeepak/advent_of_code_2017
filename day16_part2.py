13b207bb7c654a97b8e8594780dc1943d0329549
Deepak Yadav <trigodeepak@gmail.com>
2017-12-17 21:49:57 +0530

    Create day16_part2.py

---
+++ day16_part2.py
@@ -0,0 +1,29 @@
+with open('input.txt') as f:
+    lines = f.readline()
+    lines = lines.strip().split(',')
+    i = ord('a')
+    l = ord('p')
+    s = []
+    while(i<=l):
+        s.append(chr(i))
+        i+=1
+    initial = list(s)
+    for i in xrange(10000000%60):
+        for a in lines:
+            if a[0] == 'x':
+                b = a.split('/')
+                x = int(b[0][1:])
+                y = int(b[1])
+                t = s[x]
+                s[x] = s[y] 
+                s[y] = t
+            elif a[0] == 'p':
+                k = s.index(a[3])
+                s[s.index(a[1])] = a[3]
+                s[k] = a[1]
+            elif a[0] == 's':
+                x = int(a[1:])
+                s = s[-x:]+s[:-x]
+        if s == initial:
+            print "same at",i
+    print ''.join(s)
